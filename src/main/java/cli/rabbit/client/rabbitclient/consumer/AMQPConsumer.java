bee19af13135557f8d59807eb15d7dbdf98330d2
sadeq <sdqSafdari@gmail.com>
2023-10-29 16:08:36 +0330

    add verbose log capability

--- src/main/java/cli/rabbit/client/rabbitclient/consumer/AMQPConsumer.java
+++ src/main/java/cli/rabbit/client/rabbitclient/consumer/AMQPConsumer.java
@@ -2,19 +2,26 @@ package cli.rabbit.client.rabbitclient.consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 @Component
 public class AMQPConsumer implements MessageListener {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Boolean verbose;
+    public AMQPConsumer(@Value("${verbose}")Boolean verbose){
+        this.verbose=verbose;
+    }
 
     @Override
     public void onMessage(Message message) {
@@ -26,6 +33,9 @@ public class AMQPConsumer implements MessageListener {
             properties.put("payload",new String(payload, contentEncoding==null ? "UTF-8" : contentEncoding));
             properties.put("exchange",messageProperties.getReceivedExchange());
             properties.put("routing_key",messageProperties.getReceivedRoutingKey());
+            if (verbose){
+                MDC.putCloseable("verbose",this.constructLogMessage(messageProperties.getHeaders()));
+            }
             logger.info(this.constructLogMessage(properties));
         } catch (UnsupportedEncodingException e) {
             logger.error("content encoding not supported");
@@ -47,10 +57,10 @@ public class AMQPConsumer implements MessageListener {
         MessageListener.super.onMessageBatch(messages);
     }
 
-    private String constructLogMessage(Properties properties){
+    private String constructLogMessage(Map properties){
         StringBuilder stringBuilder = new StringBuilder();
         properties.forEach((k,v)-> {
-            String entry = String.format("%s=%s", k, v);
+            String entry = String.format("%s=%s", k.toString(), v.toString());
             stringBuilder.append(entry).append(" ");
         });
         return stringBuilder.toString();
