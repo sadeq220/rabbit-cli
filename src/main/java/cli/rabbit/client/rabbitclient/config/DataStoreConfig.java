ae481c496b2341486ffb23a9092b0c59487f202e
sadeq <sdqSafdari@gmail.com>
2023-11-05 12:51:39 +0330

    config data directory

--- src/main/java/cli/rabbit/client/rabbitclient/config/DataStoreConfig.java
+++ src/main/java/cli/rabbit/client/rabbitclient/config/DataStoreConfig.java
@@ -1,21 +1,46 @@
 package cli.rabbit.client.rabbitclient.config;
 
+import cli.rabbit.client.rabbitclient.RabbitClientApplication;
+
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 public class DataStoreConfig {
     @Bean
     public DataSource connectionConfiguration(){
+        Path dataDirectoryPath = this.getDataDirectoryPath();
+        Path sqliteDatabasePath = dataDirectoryPath.resolve("rabbit-consumed-messages.sqlite").toAbsolutePath();
+
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.sqlite.JDBC");
-        dataSourceBuilder.url("jdbc:sqlite:rabbit-consumed-messages.sqlite");
+        dataSourceBuilder.url("jdbc:sqlite:"+sqliteDatabasePath);
         dataSourceBuilder.username("sa");
         dataSourceBuilder.password("sa");
         return dataSourceBuilder.build();
+    }
+    private Path getDataDirectoryPath(){
+        try {
+            String jarFilePath = RabbitClientApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            if (jarFilePath.contains("!")) {
+                jarFilePath = jarFilePath.substring(0, jarFilePath.indexOf('!'));
+                jarFilePath=new URL(jarFilePath).getPath();
+            }
+            Path dataDirectoryPath = Path.of(jarFilePath).normalize().getParent().getParent().resolve("data");
+            if (Files.notExists(dataDirectoryPath)) {
+                Files.createDirectory(dataDirectoryPath);
+            }
+            return dataDirectoryPath;
+        } catch (IOException e) {
+            throw new RuntimeException("couldn't create a data directory!",e);
+        }
     }
 
 
